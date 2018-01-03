3c054eec25c5e63d74403ec5224d965162c90b1f
Andre Vieira Goncalves <andrevieira@brq.com>
2018-01-03 11:41:02 -0200

    tela de login

--- app/src/main/java/andre/com/whatsapp/activity/MainActivity.java
+++ app/src/main/java/andre/com/whatsapp/activity/MainActivity.java
@@ -1,9 +1,16 @@
-package andre.com.whatsapp;
+package andre.com.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import andre.com.whatsapp.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    //private DatabaseReference referenceFirebase = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
