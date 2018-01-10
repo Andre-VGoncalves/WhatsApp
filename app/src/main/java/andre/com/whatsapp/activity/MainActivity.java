da5fde0a4d9378984efc95b80bc3bd47d8d1bbef
Andre Vieira Goncalves <andrevieira@brq.com>
2018-01-10 13:41:56 -0200

    toolbar concluido

--- app/src/main/java/andre/com/whatsapp/activity/MainActivity.java
+++ app/src/main/java/andre/com/whatsapp/activity/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -26,10 +29,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        autenticacao = ConfigFirebase.getFirebaseAutenticacao();
+
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("DedeZap");
         setSupportActionBar(toolbar);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemSair:
+                deslogarUser();
+                return true;
+            case R.id.itemConfig:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void deslogarUser(){
+        autenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
